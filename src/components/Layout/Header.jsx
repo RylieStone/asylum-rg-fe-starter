4d6e38a97e18d6b5ae24f31e9877839ee06d63f5
Rylie <rylie.stone2005@gmail.com>
2024-07-20 06:46:21 -0400

    made changes to display profile when signed in and made it disappear when signed out as well as putting the auth button

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -3,10 +3,12 @@ import { Image } from 'antd';
 import { Link } from 'react-router-dom';
 import Logo from '../../styles/Images/WhiteLogo.png';
 import { colors } from '../../styles/data_vis_colors';
-
+import AuthButton from '../common/authentication-button';
+import { useAuth0 } from '@auth0/auth0-react';
 const { primary_accent_color } = colors;
-
 function HeaderContent() {
+  const {user} = useAuth0();
+  console.log(user);
   return (
     <div
       style={{
@@ -25,9 +27,13 @@ function HeaderContent() {
         <Link to="/" style={{ color: '#E2F0F7', paddingRight: '75px' }}>
           Home
         </Link>
-        <Link to="/graphs" style={{ color: '#E2F0F7' }}>
+        <Link to="/graphs" style={{ color: '#E2F0F7', paddingRight: '75px' }}>
           Graphs
         </Link>
+        {user !== undefined ? <Link to="/profile" style={{ color: '#E2F0F7', paddingRight: '75px'}}>
+        Profile</Link> : null}
+
+        <AuthButton />
       </div>
     </div>
   );
