0d5558ae274b7015de3ffa623a8733a53e5870bf
Rylie <rylie.stone2005@gmail.com>
2024-07-20 06:44:38 -0400

    added auth0-with-history and added a new route

--- src/index.jsx
+++ src/index.jsx
@@ -6,11 +6,11 @@ import {
   // useHistory,
   Switch,
 } from 'react-router-dom';
-
+import Auth0ProviderWithHistory from './auth/auth0-provider-with-history';
 import 'antd/dist/antd.less';
 import { NotFoundPage } from './components/pages/NotFound';
 import { LandingPage } from './components/pages/Landing';
-
+import Profile from './components/pages/profile/profile';
 import { FooterContent, SubFooter } from './components/Layout/Footer';
 import { HeaderContent } from './components/Layout/Header';
 
@@ -28,11 +28,13 @@ const { primary_accent_color } = colors;
 const store = configureStore({ reducer: reducer });
 ReactDOM.render(
   <Router>
-    <Provider store={store}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </Provider>
+    <Auth0ProviderWithHistory>
+      <Provider store={store}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </Provider>
+    </Auth0ProviderWithHistory>
   </Router>,
   document.getElementById('root')
 );
@@ -54,6 +56,7 @@ export function App() {
       <Switch>
         <Route path="/" exact component={LandingPage} />
         <Route path="/graphs" component={GraphsContainer} />
+        <Route path="/profile" component={Profile} />
         <Route component={NotFoundPage} />
       </Switch>
       <Footer
